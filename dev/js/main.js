b050c03b841499747c5e898e19fe57720864728a
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-03-24 14:21:44 -0400

    This is getting really awesome.

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,13 +1,22 @@
-function Item(positionX, positionY, oncollide) {
+var context, character, itemList, canvas;
+var scale = 10;
+
+
+function Item(positionX, positionY, pointValue) {
 	this.positionX = positionX;
 	this.positionY = positionY;
-	this.oncollide = oncollide;
+	this.pointValue = pointValue;
+}
+
+Item.prototype.remove = function() {
+
 }
 
 Item.prototype.checkCollisions = function(character) {
 	if (character.positionX - 5 <= this.positionX && character.positionX + 5 >= this.positionX) {
 		if (character.positionY - 5 <= this.positionY && character.positionY + 5 >= this.positionY) {
-			this.oncollide(character);
+			character.alterScore(this.pointValue);
+			this.remove();
 		}
 	}
 }
@@ -17,9 +26,8 @@ function Character() {
 	this.velocityY = 0;
 	this.positionX = 0;
 	this.positionY = 0;
-	this.tickInterval = function() {
-		return -1;
-	}
+	this.tickTimeout = 0;
+	this.score = 0;
 	this.printStats = function() {
 		var _this = this;
 		Object.keys(this).forEach(function(key) {
@@ -28,35 +36,45 @@ function Character() {
 	  	}
 		});
 	}
+	this.context;
 }
 
-Character.prototype.tick = function(context) {
-	context.strokeStyle = "#fff";
-	context.fillStyle = "#fff";
-	context.beginPath();
-	context.arc(this.positionX, 164-this.positionY, 3, 0, 2 * Math.PI, false);
-	context.fill();
-	this.velocityY -= 1;
+function tick() {
+	console.log(1);
+}
+
+Character.prototype.alterScore = function(amount) {
+	this.score += amount;
+}
+
+Character.prototype.tick = function() {
+	console.log(this.positionX.toFixed(1), this.positionY.toFixed(1));
+	this.context.strokeStyle = "#fff";
+	this.context.fillStyle = "#fff";
+	this.context.beginPath();
+	this.context.arc(this.positionX, canvas.height-this.positionY, scale+1, 0, 2 * Math.PI, false);
+	this.context.fill();
+	this.velocityY -= scale;
 	this.velocityX *= .95;
-	this.velocityX = parseInt(this.velocityX.toFixed(2));
+	this.velocityX = parseFloat(this.velocityX.toFixed(2));
+	this.velocityX = Math.abs(this.velocityX) <= scale/5 ? 0 : this.velocityX;
 	this.positionX += this.velocityX;
 	this.positionY += this.velocityY;
 	if (this.positionY < 0) {
 		this.positionY = 0;
 		this.velocityY = 0;
 	} else {
-		if (this.positionY > 164) {
+		if (this.positionY > canvas.height - scale) {
 			this.positionY = 0;
-		} else {
-			if (this.positionX > 264 || this.positionX < 0) {
-				this.positionX = this.positionX < 0 ? 264 : 0;
-			}
 		}
 	}
-	context.fillStyle = "#000";
-	context.beginPath();
-	context.arc(this.positionX, 164-this.positionY, 2, 0, 2 * Math.PI, false);
-	context.fill();
+	if (this.positionX > canvas.width || this.positionX < 0) {
+		this.positionX = this.positionX < 0 ? canvas.width - scale : scale;
+	}
+	this.context.fillStyle = "#000";
+	this.context.beginPath();
+	this.context.arc(this.positionX, canvas.height-this.positionY, scale, 0, 2 * Math.PI, false);
+	this.context.fill();
 	//this.printStats();
 }
 
@@ -64,39 +82,36 @@ Character.prototype.handleVelocityChange = function(deltaX,deltaY) {
 	console.log(deltaX,deltaY);
 	this.velocityX += deltaX;
 	this.velocityY += deltaY;
-	// dthis.printStats();
+	//this.printStats();
 }
 
-Character.prototype.startTicking = function(context) {
-	console.log(context);
-	var x = setInterval(this.tick,75,context);
-	this.tickInterval = function() {
-		return x;
-	}
-}
-
-var character = new Character();
-var itemsList = [new Item(0,0,console.log)];
+character = new Character();
+itemsList = [new Item(0,0,2)];
 
 
 window.onload = function() {
 	canvas = document.createElement("canvas");
 	document.body.appendChild(canvas);
-	canvas.width = 264;
-	canvas.height = 164;
+	canvas.width = 264*scale;
+	canvas.height = 164*scale;
 	canvas.style.width = "100%";
 	canvas.style.height = "62%";
 	context = canvas.getContext('2d');
-	character.startTicking(context);
 	document.onkeydown = function(e) {
 		if (e.code == 'KeyW' || e.code == 'ArrowUp') {
-			character.handleVelocityChange(0,5);
+			character.handleVelocityChange(0,5*scale);
 		}
 		if (e.code == 'KeyA' || e.code == 'ArrowLeft') {
-			character.handleVelocityChange(-5,0);
+			character.handleVelocityChange(0-(5*scale),0);
 		}
 		if (e.code == 'KeyD' || e.code == 'ArrowRight') {
-			character.handleVelocityChange(5,0);
+			character.handleVelocityChange(5*scale,0);
 		}
 	}
+	character.context = context;
+	setInterval(tickCharacter,75);
+}
+
+function tickCharacter() {
+	character.tick()
 }
